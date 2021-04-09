8a5cf96d3fbeef986849d92ee00d5086bbf55582
Jay Desai <jaydesai571@gmail.com>
2021-04-09 00:43:21 -0400

    Login and Register completed between Reacr redux and Node modules

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
-import { Link }  from 'react-router-dom';
+import { Link, Redirect }  from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] =useState({
         email: '',
         password: ''
@@ -13,8 +16,13 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('Success');
+        login({ email, password });
     };
+
+    //Redirected if logged in
+    if(isAuthenticated) {
+        return <Redirect to="/dashboard" />
+    }
 
     return (
         <Fragment>
@@ -44,5 +52,15 @@ const Login = () => {
             </p>
         </Fragment>
     )
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
 }
-export default Login
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useState} from 'react';
-import { Link }  from 'react-router-dom';
+import { Link, Redirect }  from 'react-router-dom';
 import { connect } from 'react-redux';
 import {setAlert} from '../../actions/alert';
 import {register} from '../../actions/auth';
 import propTypes from 'prop-types';
 
-const Register = ({setAlert, register}) => {
+
+const Register = ({setAlert, register, isAuthenticated}) => {
     const [formData, setFormData] =useState({
         name: '',
         email: '',
@@ -25,6 +26,10 @@ const Register = ({setAlert, register}) => {
             register({name, email, password});
         }
     };
+
+    if(isAuthenticated) {
+        return <Redirect to="/dashboard" />
+    }
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -71,6 +76,12 @@ const Register = ({setAlert, register}) => {
 
 Register.propTypes = {
      setAlert: propTypes.func.isRequired,
-     register: propTypes.func.isRequired
+     register: propTypes.func.isRequired,
+     isAuthenticated: propTypes.bool
 };
-export default connect(null, { setAlert, register } )(Register);
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register } )(Register);
