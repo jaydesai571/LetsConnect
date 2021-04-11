26eb8ddd066c46bedda3d4078d9439bdf0a50b17
Jay Desai <jaydesai571@gmail.com>
2021-04-11 17:48:50 -0400

    Base fpr the Dashboard data not completely working.

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -8,7 +8,7 @@ const initialState = {
     error: {}
 }
 
-export default function(state = initialState, action) {
+export default function profileChecking (state = initialState, action) {
     const { type, payload } =action;
 
     switch(type) {
@@ -17,13 +17,20 @@ export default function(state = initialState, action) {
                 ...state,
                 profile: payload,
                 loading: false
-            }
+            };
         case PROFILE_ERROR:
             return {
                 ...state,
                 error: payload,
                 loading: false
-            }
+            };
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
+                loading: false
+            };
         default:
             return state
     }
